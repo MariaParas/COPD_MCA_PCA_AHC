f675df54a20b5fe53f6e4d8fe1e214d2f8af4bf3
Maria <MariaParas@users.noreply.github.com>
2017-09-05 05:24:32 +0300

    Update Step2_Final_mca_drugs+dis.R

--- R_code/Step2_Final_mca_drugs+dis.R
+++ R_code/Step2_Final_mca_drugs+dis.R
@@ -3,15 +3,7 @@ dim(drugs)
 head(drugs)
 res.mca <- MCA(drugs,ncp=8, graph=FALSE)
 explor(res.mca)
-#1	25.0	25.0
-#2	13.4	38.4
-#3	12.5	50.9
-#4	11.2	62.1
-#5	11.2	73.3
-#6	9.7	83.0
-#7	8.9	91.9
-#8	8.1	100.0
-#first 5 component expain 73.3 of variance
+
 ind <- get_mca_ind(res.mca)
 head(ind)
 head(ind$coord)
@@ -19,11 +11,6 @@ drug=res.mca$ind$coord
 summary(res.mca, nb.dec = 2, ncp = 8)
 dev.off()
 
-# The plot above helps to identify variables that are the most 
-# correlated with each dimension. The squared correlations between variables and the dimensions are used as coordinates.
-
-# It can be seen that, the drugs mucolytics and laba lama are the most correlated with dimension 1. Similarly, saba sama sama are the most correlated with dimension 2.
-# saba contributes the same
 var= get_mca_var(res.mca)
 var
 # coordinates
@@ -39,9 +26,7 @@ library("corrplot")
 corrplot(var$contrib, is.corr = FALSE)
 # Contributions of variables on Dim.1
 fviz_contrib(res.mca, choice = "var", axes = 1)
-# Control category point colors using their contribution
-# Possible values for the argument col.row are :
-# "cos2", "contrib", "coord", "x", "y"
+
 fviz_mca_var(res.mca, col.var = "contrib")
 
 library("corrplot")
@@ -58,20 +43,7 @@ head(como1)
 summary(como1)
 res.mca <- MCA(como1,ncp=13, graph=FALSE)
 explor(res.mca)
-#1	15.3	15.3
-#2	10.5	25.8
-#3	9.1	34.9
-#4	8.8	43.7
-#5	7.6	51.4
-#6	7.6	59.0
-#7	7.3	66.3
-#8	6.9	73.2
-#9	6.7	79.9
-#10	6.4	86.3
-#11	5.8	92.1
-#12	5.4	97.5
-#13	2.5	100.0
-# fis=rst 8 components explain 73.2%
+
 ind <- get_mca_ind(res.mca)
 head(ind)
 head(ind$coord)
@@ -79,11 +51,6 @@ comorbid=res.mca$ind$coord
 summary(res.mca, nb.dec = 2, ncp = 2)
 dev.off()
 
-# The plot above helps to identify variables that are the most 
-# correlated with each dimension. The squared correlations between variables and the dimensions are used as coordinates.
-
-# It can be seen that, the drugs mucolytics and laba lama are the most correlated with dimension 1. Similarly, saba sama sama are the most correlated with dimension 2.
-# saba contributes the same
 var= get_mca_var(res.mca)
 var
 # coordinates
@@ -99,9 +66,6 @@ library("corrplot")
 corrplot(var$contrib, is.corr = FALSE)
 # Contributions of variables on Dim.1
 fviz_contrib(res.mca, choice = "var", axes = 1)
-# Control category point colors using their contribution
-# Possible values for the argument col.row are :
-# "cos2", "contrib", "coord", "x", "y"
 fviz_mca_var(res.mca, col.var = "contrib")
 
 library("corrplot")
